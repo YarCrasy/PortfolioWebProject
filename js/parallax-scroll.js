a26e0ad2688b9b423eda9dcd7e6981d2d236dec3
YarCrasy <108998893+YarCrasy@users.noreply.github.com>
2024-11-13 14:20:42 +0000

    FixedPanelScroll

--- js/parallax-scroll.js
+++ js/parallax-scroll.js
@@ -3,9 +3,7 @@
 //but more or less have the same scroll requirement
 //and it fits better with my web instead of doing something weird
 
-const PANELS = document.querySelectorAll(".my-panel");
-let scrollPosition = 0; // Controlar la posición del scroll
-
+let scrollPosition = 0; //actual focused panel
 
 window.onload = () => {
     //wait for the page to be totally loaded and update panels
@@ -13,28 +11,28 @@ window.onload = () => {
 };
 
 function handleScroll(event) {
+    const PANELS = document.querySelectorAll(".my-panel");
+
+    //cancel default scroll
     event.preventDefault();
-    //detect every scroll tick, in this case every 7 tick reach a new panel
-    if (window.innerHeight < 1080) {
-        scrollPosition += event.deltaY / 800;
-    }
-    else {
-        scrollPosition += event.deltaY / 700;
-    }
+    //add or substract the actual index position
+    scrollPosition += event.deltaY / 100;
     //limit scroll
     scrollPosition = Math.max(0, Math.min(PANELS.length - 1, scrollPosition));
+    window.scrollTo({ top: PANELS[scrollPosition].offsetTop, });
 
     updatePanelsPosition();
 }
 
 function updatePanelsPosition() {
-    PANELS.forEach((panel, index) => {
-        let scale = Math.max(0.5, 0.9 - Math.abs(index - scrollPosition) * 0.5);
-        let opacity = Math.max(0.5, 1 - Math.abs(index - scrollPosition) * 0.5);
+    const PANELS = document.querySelectorAll(".my-panel");
 
-        panel.style.transform = `translateY(${0}px) scale(${scale})`;
-        panel.style.opacity = opacity;
-    });
+    for (let i = 0; i < PANELS.length; i++) {
+        let scale = Math.max(0.5, 1 - Math.abs(i - scrollPosition) * 0.3);
+        let opacity = Math.max(0.5, 1 - Math.abs(i - scrollPosition) * 0.5);
+        PANELS[i].style.transform = `scale(${scale})`;
+        PANELS[i].style.opacity = opacity;
+    }
 }
 
-window.addEventListener('wheel', handleScroll);
+window.addEventListener('wheel', handleScroll, { passive: false });
