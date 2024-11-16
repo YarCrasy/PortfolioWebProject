4ab56e0a166d2976528869f4a2585c17276e2f49
YarCrasy <108998893+YarCrasy@users.noreply.github.com>
2024-11-16 13:09:05 +0000

    InprovingLanguage

--- js/add-html-components.js
+++ js/add-html-components.js
@@ -7,5 +7,27 @@ function addComponent(path, elementId) {
         .catch(error => console.error(elementId + "", error));
 }
 
-addComponent("../../html/html-components/header.html", "header");
-addComponent("../../html/html-components/footer.html", "footer");
+function addComponentScripts() {
+    const SCRIPT_PATHS = ["../../js/footer-scripts", "../../js/header-scripts"];
+    const HTML_DIV = document.getElementById("scripts");
+
+    scriptFolders.forEach(folder => {
+        fetch(folder)
+            .then(response => response.text())
+            .then(data => {
+                let parser = new DOMParser();
+                let doc = parser.parseFromString(data, 'text/html');
+                let scriptTags = doc.querySelectorAll('script');
+                scriptTags.forEach(script => {
+                    let newScript = document.createElement('script');
+                    newScript.src = script.src;
+                    HTML_DIV.appendChild(newScript);
+                });
+            })
+            .catch(error => console.error("Error loading scripts from " + folder, error));
+    });
+}
+
+addComponent("html-components/header.html", "header");
+addComponent("html-components/footer.html", "footer");
+addComponentScripts();
