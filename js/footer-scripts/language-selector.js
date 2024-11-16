e7a9201074a31ff9cd43ddcaa1c242927aba715f
YarCrasy <108998893+YarCrasy@users.noreply.github.com>
2024-11-16 15:51:45 +0000

    FixedLanguageSelector

--- js/footer-scripts/language-selector.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('language-dropdown').addEventListener('change', function() {
-    const selectedLanguage = this.value;
-    if (selectedLanguage) {
-        window.location.href = selectedLanguage;
-    }
-});
