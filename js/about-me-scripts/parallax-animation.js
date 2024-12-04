c3333d4be66f12a3303ba3f6a266053f658d2eeb
YarCrasy <108998893+YarCrasy@users.noreply.github.com>
2024-12-04 14:40:32 +0000

    addingContent

--- js/about-me-scripts/parallax-animation.js
+++ js/about-me-scripts/parallax-animation.js
@@ -6,42 +6,34 @@ function initElements() {
     const CLOUD2 = "../../imgs/parallax-imgs/clouds-n2-b-g-main.png";
     const MOON = "../../imgs/parallax-imgs/moon3.png";
 
-    const PARALLAX_1 = document.getElementById("parallax-1");
-    
-    let aux1, aux2;
-    for(let i = 0; i < 10000; i++){
-        aux1 = document.createElement("img");
-        aux1.src = CLOUD1;
-        aux1.classList.add("parallax-1");
-        aux1.style.transform = `translate(${(Math.random() - 1) * 1}vw, ${(Math.random()) * 100}vh)`;
-        PARALLAX_1.appendChild(aux1);
+}
 
-        aux2 = document.createElement("img");
-        aux2.src = CLOUD2;
-        aux2.classList.add("parallax-1");
-        aux2.style.transform = `translate(${(Math.random() - 1) * 1}vw, ${(Math.random() - 1) * 1}vh)`;
-        PARALLAX_1.appendChild(aux2);
+function createElementAtLayer(layer, imgPath) {
+    const PARALLAX_BG = document.getElementById("parallax-bg");
+    let parallaxLayer = document.createElement("div");
+    parallaxLayer.id = `parallax-${layer}`;
+    parallaxLayer.classList.add("parallax-layer");
+    PARALLAX_BG.appendChild(parallaxLayer);
+
+    let aux;
+    let posX, posY;
+    for (let i = 0; i < 100; i++) {
+        aux = document.createElement("img");
+        parallaxLayer.appendChild(aux);
+        aux.src = imgPath;
+        posX = Math.random();
+        posY = Math.random();
+        aux.style.transform = `translateX(${posX * 1}%)`;
+        aux.style.transform = `translateY(${posY * 500}%)`;
     }
-
 }
 
 function handleScroll(event) {
-    event.preventDefault();
-
-}
-
-function handleTouchStart(event) {
-    startY = event.touches[0].clientY;
-}
-
-function handleTouchEnd(event) {
 
 }
 
 
 window.addEventListener('wheel', handleScroll, { passive: false });
-window.addEventListener('touchstart', handleTouchStart, { passive: false });
-window.addEventListener('touchend', handleTouchEnd, { passive: false });
 
 initElements();
 
